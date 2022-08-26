e8e281933a342d084e15fef7114bac5df6dbdb45
nadiauj <nadiauj@ext.inditex.com>
2022-08-26 19:18:39 -0300

    Add bulma CSS

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import postcssNesting from 'postcss-nested';
 
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        postcssNesting,
+      ],
+    },
+  },
+});
