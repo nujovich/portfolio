1923f711f57c8a02ac3afde4e94cbd4c25d09e84
nadiauj <nadiauj@ext.inditex.com>
2022-08-28 11:10:47 -0300

    add download cv feature

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -1,10 +1,21 @@
+import FileSaver from 'file-saver';
+
+function downloadCV() {
+    FileSaver.saveAs(import.meta.env.BASE_URL + "/public/Nadia Ujovich Spanish CV Version v1.0.pdf",
+    "Nadia_Ujovich_CV_Spanish.pdf");
+}
+
 export function AboutMe() {
     return(
         <section id="banner">
             <div className="banner-container">
                 <h3>Nadia Ivana Ujovich</h3>
                 <h2>Fullstack Developer</h2>
-                <a href="#avatar" className="about-me-button">Sobre mi</a>
+                <button className="about-me-button" onClick={downloadCV}>
+                    <span>Descargar cv</span>
+                        &nbsp;
+                    <iconify-icon inline className="download-icon" icon="bx:download" width="20" height="20"></iconify-icon>
+                </button>
             </div>
         </section>
     )
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import postcssNesting from 'postcss-nested';
+import EnvironmentPlugin from 'vite-plugin-environment'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    EnvironmentPlugin({ BASE_URL: 'http://localhost:5173' }, { defineOn: 'import.meta.env' })
+  ],
   css: {
     postcss: {
       plugins: [
