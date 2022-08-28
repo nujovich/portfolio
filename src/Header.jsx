afae413516a20a914a32755a3c63df4ff68bd0b2
nadiauj <nadiauj@ext.inditex.com>
2022-08-28 17:02:49 -0300

    Make menu responsive with react hooks

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,24 +1,30 @@
+import { useState } from "react"
+
+
 export function Header () {
+
+  const[rightProp, setRightProp] = useState({right: "-100%"})
+
     return (
         <header>
         <img className="logo" src="/logo.svg" />
-        <input type="checkbox" id="check"/>
+        <input type="checkbox" id="check" onClick={() => setRightProp({right: "0"})}/>
         <label htmlFor="check" className="show-menu">
           <iconify-icon icon="eva:menu-outline" width="40" height="40"></iconify-icon>
         </label>
-        <nav className="menu">
+        <nav className="menu" style={rightProp}>
           <ul className="menu-items">
             <li>
-              <a href="#avatar" className="menu-link">Sobre mi</a>
+              <a href="#avatar" className="menu-link" onClick={() => setRightProp({right: "-100%"})}>Sobre mi</a>
             </li>
             <li>
-              <a href="#projects" className="menu-link">Proyectos</a>
+              <a href="#projects" className="menu-link" onClick={() => setRightProp({right: "-100%"})}>Proyectos</a>
             </li>
             <li>
-              <a href="#skills" className="menu-link">Skills</a>
+              <a href="#skills" className="menu-link" onClick={() => setRightProp({right: "-100%"})}>Skills</a>
             </li>
             <li>
-              <a href="#contact" className="menu-link">Contacto</a>
+              <a href="#contact" className="menu-link" onClick={() => setRightProp({right: "-100%"})}>Contacto</a>
             </li>
           </ul>
           <label htmlFor="check" className="hide-menu">
--- src/script.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector(".menu")
-const menuItems = document.querySelectorAll(".menu-link")
-
-menuItems.forEach(item => item.addEventListener("click", () => {
-    menu.style.right = "-100%"
-}))
