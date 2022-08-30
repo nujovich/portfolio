6a877d0578631249db535bce3813835d67d2e18f
nadiauj <nadiauj@ext.inditex.com>
2022-08-30 15:09:35 -0300

    Fix base public path

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -1,12 +1,10 @@
 import FileSaver from 'file-saver';
 
-
-const BASE_URL = import.meta.env.MODE === 'development' ? import.meta.env.LOCAL_URL : import.meta.env.PROD_URL
-const FILE_PATH = '/portfolio/Nadia Ujovich Spanish CV Version v1.0.pdf'
+const FILE_PATH = 'Nadia Ujovich Spanish CV Version v1.0.pdf'
 
 function downloadCV() {
-    console.log(BASE_URL+FILE_PATH)
-    FileSaver.saveAs(BASE_URL + FILE_PATH, "Nadia_Ujovich_CV_Spanish.pdf");
+    console.log(import.meta.env.BASE_URL + FILE_PATH)
+    FileSaver.saveAs(import.meta.env.BASE_URL + FILE_PATH, "Nadia_Ujovich_CV_Spanish.pdf");
 }
 
 export function AboutMe() {
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import postcssNesting from 'postcss-nested';
-import EnvironmentPlugin from 'vite-plugin-environment'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    EnvironmentPlugin(
-      { LOCAL_URL: 'http://localhost:5174' }, { defineOn: 'import.meta.env' },
-      { PROD_URL: 'https://nujovich.github.io' }, { defineOn: 'import.meta.env' },
-    )
-  ],
+  plugins: [react()],
   base: '/portfolio/',
   css: {
     postcss: {
